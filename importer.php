e827e93c04f143c8023f84bf48ec7849f4f823b9
Seb Lamberts <seb.lamberts@eyup.com>
2022-05-16 16:14:47 +0100

    made small change to importer.php

--- importer.php
+++ importer.php
@@ -1,14 +1,7 @@
 <?php
 
-use PennyLaneProperties\Database\APIHandler;
-use PennyLaneProperties\Database\DatabaseConnector;
-use PennyLaneProperties\Database\DatabaseImporter;
+use PennyLaneProperties\Database\{APIHandler, DatabaseConnector, DatabaseImporter};
 
 require_once "vendor/autoload.php";
 
-$db = DatabaseConnector::connectToDB();
-
-$handler = new APIHandler();
-
-$importer = new DatabaseImporter($db, $handler);
-$importer->populateDatabase();
+(new DatabaseImporter(DatabaseConnector::connectToDB(), new APIHandler))->populateDatabase();
