519aa54e1dffad6342149053253976bba07f5d46
Seb Lamberts <seb.lamberts@eyup.com>
2022-05-17 09:40:30 +0100

    removed redundant code in APIHandler

--- src/Database/APIHandler.php
+++ src/Database/APIHandler.php
@@ -11,31 +11,18 @@ class APIHandler
     public function __construct()
     {
         $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/properties.json");
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+        curl_setopt($ch, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/properties.json");
         $output = curl_exec($ch);
-
         $properties = json_decode($output, true);
 
         curl_setopt($ch, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/types.json");
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
         $output = curl_exec($ch);
-
         $types = json_decode($output, true);
 
-        $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_URL, "https://dev.io-academy.uk/resources/property-feed/statuses.json");
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
         $output = curl_exec($ch);
-
         $statuses = json_decode($output, true);
 
         curl_close($ch);
