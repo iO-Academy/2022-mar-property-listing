8a1cd09526c5b7abbd4e04076236a8dc43eda4fc
Ali McGarry <ali.mcgarry@gmail.com>
2022-05-17 16:18:47 +0100

    starting to build property page

--- displayPage.php
+++ displayPage.php
@@ -9,7 +9,9 @@ $displayHydrator = new DisplayPageHydrator();
 
 $db = DatabaseConnector::connect();
 $propertyData = $displayHydrator->getProperty($db, 'CSL123_100259');
+echo '<pre>';
 var_dump($propertyData);
+echo '</re>';
 
 ?>
 <!DOCTYPE html>
@@ -24,7 +26,23 @@ var_dump($propertyData);
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="container-fluid justify-content-center navbar">
+        <img src="Assets/pennyLaneLogo2.svg">
+    </div>
+    <div class="container">
 
+        <div class="row">
+            <div class="card mb-3">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo "{$propertyData["address_1"]}, {$propertyData["address_2"]}" ?></h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
 </html>
 
--- src/DisplayPage/DisplayPageHydrator.php
+++ src/DisplayPage/DisplayPageHydrator.php
@@ -6,15 +6,13 @@ use PDO;
 
 class DisplayPageHydrator
 {
-    public function getProperty($db, $agentRef)
+    public function getProperty(PDO $db, string $agentRef): array
     {
         $query = $db->prepare("SELECT `agent_ref`,`address_1`,`address_2`,`town`,`postcode`,`description`,`bedrooms`,`price`,`image`,`type`,`status` FROM `properties` WHERE `agent_ref` = :agent_ref;");
         $query->bindParam(":agent_ref", $agentRef);
         $query->setFetchMode(PDO::FETCH_ASSOC);
         $query->execute();
-        $result = $query->fetchAll();
-
-        return $result;
+        return $query->fetch();
     }
 }
 
