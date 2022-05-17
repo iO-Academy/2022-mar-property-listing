669d0e50f66425859cc07b44803b04386425ee82
Ali McGarry <ali.mcgarry@gmail.com>
2022-05-17 15:58:55 +0100

    Created index to display with dummy data

---
+++ src/DisplayPage/DisplayPageHydrator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PennyLaneProperties\DisplayPage;
+
+use PDO;
+
+class DisplayPageHydrator
+{
+    public function getProperty($db, $agentRef)
+    {
+        $query = $db->prepare("SELECT `agent_ref`,`address_1`,`address_2`,`town`,`postcode`,`description`,`bedrooms`,`price`,`image`,`type`,`status` FROM `properties` WHERE `agent_ref` = :agent_ref;");
+        $query->bindParam(":agent_ref", $agentRef);
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $query->execute();
+        $result = $query->fetchAll();
+
+        return $result;
+    }
+}
+
